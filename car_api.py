a926ddf24d2301c02256a9d53787f7950e2b0e60
Axel Schwarz <A11952633@t0009e4c57.fritz.box>
2023-07-18 15:07:27 +0200

    test data for db

--- car_api.py
+++ car_api.py
@@ -9,26 +9,57 @@ client = MongoClient('mongodb://localhost:27017/')
 database = client['car_repair_shop']
 cars_collection = database['cars']
 
+# Example data
+example_cars = [
+    
+    {
+        'id': '1',
+        'make': 'Honda',
+        'model': 'Civic',
+        'year': 2020
+    },
+    {
+        'id': '2',
+        'make': 'Ford',
+        'model': 'Mustang',
+        'year': 2022
+    },
+    {
+        'id': '3',
+        'make': 'Toyota',
+        'model': 'Camry',
+        'year': 2021
+    }
+]
+
+# Insert the example cars into the collection
+cars_collection.insert_many(example_cars)
+
+# Test if all cars from the collection are in
+cursor = cars_collection.find()
+for car in cursor:
+    print(car)
+
 class Car(BaseModel):
     id: str
     make: str
     model: str
     year: int
 
-# GET /cars - Retrieve all cars
+# Retrieve cars
 @app.get('/cars')
 def get_all_cars():
     cars = cars_collection.find()
     return list(cars)
 
-# POST /cars - Create a new car
+#  Create a new car
 @app.post('/cars')
 def create_car(car: Car):
     car_data = car.model_dump()
     cars_collection.insert_one(car_data)
     return {'message': 'created a car'}
 
-# GET /cars/{id} - Retrieve a specific car
+# Retrieve a car by id
 @app.get('/cars/{car_id}')
 def get_car(car_id: str):
     car = cars_collection.find_one({'id': car_id})
@@ -36,7 +67,7 @@ def get_car(car_id: str):
         return car
     raise HTTPException(status_code=404, detail='Car not found')
 
-# PUT /cars/{id} - Update an existing car
+# Update an existing car by id
 @app.put('/cars/{car_id}')
 def update_car(car_id: str, updated_car: Car):
     car_data = updated_car.model_dump()
@@ -45,7 +76,7 @@ def update_car(car_id: str, updated_car: Car):
         return {'message': 'updated car'}
     raise HTTPException(status_code=404, detail='Car not found')
 
-# DELETE /cars/{id} - Delete a specific car
+# Delete a specific car by id
 @app.delete('/cars/{car_id}')
 def delete_car(car_id: str):
     result = cars_collection.delete_one({'id': car_id})
